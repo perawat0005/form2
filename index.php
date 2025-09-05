2b4c967db0689cab45d769b2bb91691f1c602670
PoTAE <perawat2553@hotmail.com>
2025-09-05 15:25:44 +0700

    update file

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
                 </figure>
                 <div class="card-body">
                     <span class="font-bold text-xl">
-                        โครงการประชุมเพื่อทบทวนแผนยุทธศาสตร์ แผนการเงินการคลัง และจัดทำแผนปฏิบัติการ ประจำปีงบประมาณ 2568 
+                        โครงการประชุมเพื่อทบทวนแผนยุทธศาสตร์ แผนการเงินการคลัง และจัดทำแผนปฏิบัติการ ประจำปีงบประมาณ 2568
                         ในวันที่ 10 กันยายน 2568 ณ ห้องประชุม Okamod โรงพยาบาลห้วยราช อ.ห้วยราช จ.บุรีรัมย์
                     </span>
 
@@ -162,5 +162,89 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript" src="js/controll.js"></script>
 
+<script>
+    $(document).ready(function() {
+
+
+
+        let checkValueArr = ""
+        $(document).on('change', 'input[name="radio-1"]', function() {
+            let chackValue = $('input[name="radio-1"]:checked').val();
+            let cObject = {
+                'check': chackValue
+            }
+
+            checkValueArr = chackValue
+        })
+
+
+
+
+        $('#formData').submit(function(e) {
+            e.preventDefault()
+
+            let pname = $('#pname').val();
+            let fname = $('#fname').val();
+            let lname = $('#lname').val();
+            let position = $('#position').val();
+            let depart = $('#depart').val();
+            /* let depart_name = $('#depart_name').val(); */
+            let check = checkValueArr;
+
+            console.log(depart)
+
+            if (pname == "" || fname == "" || lname == "") {
+                Swal.fire('กรุณาระบุชื่อ-นามสกุล', '', 'warning');
+            } else if (position == "") {
+                Swal.fire('กรุณาระบุตำแหน่ง', '', 'warning');
+            } else if (depart == "") {
+                Swal.fire('กรุณาเลือกหน่วยงาน', '', 'warning');
+            } else if (check == "") {
+                Swal.fire('กรุณาระบุความประสงค์', '', 'warning');
+            } else {
+                Swal.fire({
+                    icon: 'info',
+                    title: 'ต้องการบันทึกข้อมูลใช่หรือไม่',
+                    showCancelButton: true,
+                    cancelButtonText: 'ยกเลิก',
+                    confirmButtonText: 'บันทึก',
+                    closeOnConfirm: false,
+                }).then(function(result) {
+                    if (result.isConfirmed) {
+                        $.ajax({
+                            url: 'add.php',
+                            method: 'post',
+                            data: {
+                                "pname": pname,
+                                "fname": fname,
+                                "lname": lname,
+                                "position": position,
+                                "depart": depart,
+                                /* "depart_name": depart_name, */
+                                "check": check
+                            },
+                            success: function(result) {
+                                if (result == 200) {
+                                    Swal.fire('ตอบรับสำเร็จ', '', 'success').then((res) => {
+                                        if (res.isConfirmed) {
+                                            location.reload();
+                                        }
+                                    })
+                                } else {
+                                    Swal.fire('เกิดข้อผิดพลาด', '', 'warning');
+                                }
+                            }
+                        })
+                    }
+                })
+            }
+
+
+
+
+
+        })
+    })
+</script>
 
 </html>
